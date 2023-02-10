f0e37e336a471340cfd2967adbadb3c9a763237b
phamduy0165 <75572336+phamduy0165@users.noreply.github.com>
2023-02-10 19:56:54 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Box from './Box';
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <Box />
+      </div>
+    );
+  }
+}
+
+export default App;
